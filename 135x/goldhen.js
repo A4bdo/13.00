19bc3d5cea31f1dbb7ee962c77141a8f6167e981
Abdo <hkh.k382@gmail.com>
2026-09-22 19:41:47 +0300

    ME4OO

--- 135x/goldhen.js
+++
@@ -1,6 +0,0 @@
-// ME4OO PS STORE - GoldHEN v3.x Payload for 13.52
-function loadPayload() {
-    console.log("Injecting GoldHEN into PlayStation 4 memory...");
-    // الأكواد البرمجية المترجمة لملف الـ bin ليعمل أوفلاين
-}
-loadPayload();
